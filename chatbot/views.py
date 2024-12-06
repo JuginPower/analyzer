b1a609352da4de4071f69aa6961fd72839fc864c
juginpower <juginpower90@gmail.com>
2024-12-06 23:50:31 +0100

    gui: try bitsandbytes

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from threading import Lock
 
+
+model_name = "EleutherAI/gpt-j-6B"
+bnb_config = BitsAndBytesConfig(load_in_8bit=True)
 
 class ModelSingleton:
     """
-    Singleton-Pattern für Lazy Loading des Modells und Tokenizers.
+    Singleton-Pattern mit Cache und Lazy Loading.
     """
     _model = None
     _tokenizer = None
+    _lock = Lock()  # Thread-Safe Singleton Initialisierung
 
     @classmethod
     def get_model(cls):
-        if cls._model is None:
-            cls._tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
-            cls._model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+        # Sicherstellen, dass das Modell nur einmal geladen wird
+        with cls._lock:  # Sperren, um Threading-Probleme zu vermeiden
+            if cls._model is None:
+                cls._tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+                cls._model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config, device_map="cpu")
         return cls._model, cls._tokenizer
 
 
 def load_chat_view(request):
-    # Lade das Template, um den Chatbot anzuzeigen
+    # Lade das Template für Chatbot
     return render(request, 'chatbot/chat.html')
 
 
@@ -29,7 +36,7 @@ def chatbot_response(request):
     if not user_input:
         return JsonResponse({"response": "Bitte geben Sie eine Frage ein."})
 
-    # Lazy Loading und Singleton sicherstellen
+    # Lazy Loading mit Singleton und sicherstellen, dass nur einmal geladen wird
     model, tokenizer = ModelSingleton.get_model()
 
     # Tokenisieren und KI-Response generieren
