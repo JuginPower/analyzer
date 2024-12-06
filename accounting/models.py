c2651b033d227bee85853f33e60588b868886471
juginpower <juginpower90@gmail.com>
2024-12-06 08:58:50 +0100

    gui: starte neue App chatbot

--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Keyword(models.Model):
+
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="keywords")
+
+    def __str__(self) -> str:
+        return str(self.name)
+
--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Hooking AdminSite instances into your URLconf -> Diese Doku beschreibt wie ich eigene Models in die AdminSite resgistriere.
+# Schritt davor ist wichtig um mit dem Vorhaben wie man die Administratoroberfläche von Django aktiviert, verwendet und anpasst.
