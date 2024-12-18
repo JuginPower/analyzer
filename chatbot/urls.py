194405f2620a26433c886f10f0dc24cdfa801fb8
juginpower <juginpower90@gmail.com>
2024-12-18 16:00:15 +0100

    accounting: delete chatbot Teil 2

--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.load_chat_view, name='chat_view'),
-    path('response/', views.chatbot_response, name='chatbot_response'),
-]
